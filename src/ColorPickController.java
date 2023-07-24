f2977b3da078aea6529bd39bb3701830722b0541
Inbar767 <118209251+Inbar767@users.noreply.github.com>
2023-07-24 21:18:10 +0300

    Delete ColorPickController.java

--- src/ColorPickController.java
+++
@@ -1,8 +0,0 @@
-
-public class ColorPickController {
-	private final static String COLOR[] = {"#87AFC7", "#659EC7", "#728FCE",
-			"#C6AEC7", "#C6AEC7", "#C6AEC7",
-			"#ff9eb9", "#F778A1", "#F778A1"};
-	
-
-}
